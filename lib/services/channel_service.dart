76af9dd0ff187f1c00e9851dbfe53607dabf1078
sabreys <trinityforce37@gmail.com>
2023-11-07 13:37:46 +0300

    feat: Channel page and Channel service created.

---
+++ lib/services/channel_service.dart
@@ -0,0 +1,11 @@
+import '../models/Channel.dart';
+
+class ChannelService {
+
+  List<Channel> channels = [
+    Channel(title: "Tech", description: "Technology chat",path: "tech"),
+    Channel(title: "Off Topic", description: "General chat",path: "offtopic"),
+    Channel(title: "Sport", description: "Sport chat",path: "sport"),
+  ];
+
+}
